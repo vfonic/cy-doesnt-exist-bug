0a0e53e70a1b2574adb17b4fd6088d7de12c5cf8
Cory House <housecor@gmail.com>
2020-04-09 12:15:41 -0500

    Create bug example

---
+++ cypress/integration/app.spec.js
@@ -0,0 +1,8 @@
+/// <reference types="cypress" />
+
+it("should throw an error when text does not exist", () => {
+  cy.visit("http://localhost:3000");
+  // This passes. It should fail. This text exists.
+  // Per this example, this approach for testing non-existence should work: https://testing-library.com/docs/cypress-testing-library/intro#examples
+  cy.findByText("save to reload.").should("not.exist");
+});
